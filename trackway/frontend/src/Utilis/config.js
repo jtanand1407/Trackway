bb5d34727661372e37797a0c14ecc6564d362194
Jatin Anand <89997611+jtanand1407@users.noreply.github.com>
2023-12-25 16:24:22 +0530

    Update config.js
    added /api/v1

--- trackway/frontend/src/Utilis/config.js
+++ trackway/frontend/src/Utilis/config.js
@@ -1,5 +1,5 @@
 // export const BASE_URL = 'https://trackway.onrender.com'
 
-export const BASE_URL = 'https://trackway-backend.vercel.app'
+export const BASE_URL = 'https://trackway-backend.vercel.app/api/v1'
 
 // http://localhost:4000/api/v1
